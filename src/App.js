82139d4759f3b590af430f9355099581cb173cce
StefanRudareanu <sneeze.lil@yahoo.com>
2022-06-11 17:58:15 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {FacebookAuthProvider,getAuth,onAuthStateChanged} from "firebase/auth"
+import SocialMediaProvider from './AuthenticationFacebook'
+import {useState} from "react";
+import {uploadImage} from './ImageUpload';
+import {init} from "./firebase-configuration";
+import {state} from "./AuthenticationFacebook"
 function App() {
+var logged;
+init();
+const provider=new FacebookAuthProvider();
+const auth= getAuth(init());
+
+const handleclick=async ()=>{
+     await  SocialMediaProvider( provider);
+     // await(logged.getIdToken(true));
+     return console.log(logged);}
+const [imageUpload,setImageUpload]= useState(null);
+const handlephotos= async () =>{
+    if(state!=null){
+   var photo= uploadImage(imageUpload,  state.uid);
+   console.log(photo);}
+else{
+    alert("User not conected");
+    }}
+    onAuthStateChanged(auth,(state)=>{
+        if(state){
+
+        }
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div>
+       <button onClick={handleclick}>Sing in with Faceboook</button>
+         <div>
+             <input type="file" onChange={(event)=>{setImageUpload(event.target.files)}}/>
+             <button onClick={handlephotos}>UploadPhoto</button>
+
+         </div>
+     </div>
+
   );
 }
 
